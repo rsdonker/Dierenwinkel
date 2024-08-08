96fb50fea99fdae7223d0db3bff9a5637feefcad
RSDonker <rsdonker@outlook.com>
2024-08-08 10:49:54 +0200

    Added DonkeyClass
    Iiaa!

--- Main.py
+++ Main.py
@@ -1,11 +1,14 @@
-from Dog import DogClass
-from Cat import CatClass
-from Fox import FoxClass
+from Dog    import DogClass
+from Cat    import CatClass
+from Fox    import FoxClass
+from Donkey import DonkeyClass
 
 felix = DogClass("Felix")
 mika  = CatClass("Mika")
 peter = FoxClass("Peter")
+tommy = DonkeyClass("Tommy")
 
 felix.sound()
 mika.sound()
 peter.sound()
+tommy.sound()
