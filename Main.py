fcac222fd81b7d19fee6fbe00d9da3051248cb99
RSDonker <rsdonker@outlook.com>
2024-08-08 10:58:28 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,12 +3,12 @@ from Cat    import CatClass
 from Fox    import FoxClass
 from Donkey import DonkeyClass
 
-felix = DogClass("Felix")
+doodle = DogClass("Doodle")
 mika  = CatClass("Mika")
 peter = FoxClass("Peter")
 tommy = DonkeyClass("Tommy")
 
-felix.sound()
+doodle.sound()
 mika.sound()
 peter.sound()
 tommy.sound()
