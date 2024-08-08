11296ee3bb38d5dd52c62f865fa7d275f364f225
rsdonker <53272875+rsdonker@users.noreply.github.com>
2024-08-08 14:40:41 +0200

    Revert "Added Flappie the RabbitClass"

--- Main.py
+++ Main.py
@@ -2,13 +2,11 @@ from Dog    import DogClass
 from Cat    import CatClass
 from Fox    import FoxClass
 from Donkey import DonkeyClass
-from Rabbit import RabbitClass
 
 goofy = DogClass("Goofy")
 mika  = CatClass("Mika")
 peter = FoxClass("Peter")
 tommy = DonkeyClass("Tommy")
-flappie = RabbitClass("Flappie")
 # this is a comment
 # this is another comment
 
@@ -16,4 +14,3 @@ goofy.sound()
 mika.sound()
 peter.sound()
 tommy.sound()
-flappie.sound()
--- Rabbit.py
+++
@@ -1,6 +0,0 @@
-class RabbitClass:
-    def __init__(self, name_:str) -> None:
-        self.name = name_
-
-    def sound(self) -> None:
-        print("Snif snif")
