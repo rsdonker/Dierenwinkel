44e4b1267324b8857165c69002d6d4ff66122205
rsdonker <53272875+rsdonker@users.noreply.github.com>
2024-08-08 10:01:39 +0200

    Update Main.py
    Added the FoxClass

--- Main.py
+++ Main.py
@@ -1,7 +1,11 @@
 from Dog import DogClass
 from Cat import CatClass
+from Fox import FoxClass
+
 felix = DogClass("Felix")
 mika  = CatClass("Mika")
+peter = FoxClass("Peter")
 
 felix.sound()
 mika.sound()
+peter.sound()
